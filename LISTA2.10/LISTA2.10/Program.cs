a2aab0ffab6b0c2010e442bdedf44e213e529106
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-08-13 11:15:06 -0300

    Add files via upload

---
+++ LISTA2.10/LISTA2.10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA2._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota P1:");
+            double P1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota P2:");
+            double P2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (P1 + 2 * P2) / 3;
+
+            if (media >= 5)
+                Console.WriteLine("Aprovado");
+            else
+                Console.WriteLine("Reprovado");
+
+            Console.WriteLine("Média: " + media);
+
+        }
+    }
+}
